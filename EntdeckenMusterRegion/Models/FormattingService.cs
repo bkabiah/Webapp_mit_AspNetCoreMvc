e53cff0de59e76cccf253b8bdbe34a6372a8097d
kabiah <bkabiah@yahoo.fr>
2021-12-27 00:55:51 +0100

    First commit

---
+++ EntdeckenMusterRegion/Models/FormattingService.cs
@@ -0,0 +1,16 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EntdeckenMusterRegion.Models
+{
+    public class FormattingService
+    {
+        public string AsReadableDate(DateTime date)
+        {
+            return date.ToString("D");
+        }
+    }
+}
---
+++ EntdeckenMusterRegion/Models/MonthlySpecial.cs
@@ -0,0 +1,11 @@
+﻿
+namespace EntdeckenMusterRegion.Models
+{
+    public class MonthlySpecial
+    {
+        public string Key { get; set; }
+        public string Name { get; set; }
+        public string Type { get; set; }
+        public int Price { get; set; }
+    }
+}
